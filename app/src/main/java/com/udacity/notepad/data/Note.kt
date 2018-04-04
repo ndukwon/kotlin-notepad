47aeae37c7488b7577bc7a5713a83753ed31f534
ndukwon <ndukwon@gmail.com>
2018-04-04 12:59:45 +0900

    Convert Note class to data class
    - Data class in Kotlin supports
      + Identity: equal()
      + Copy: copy()
      + Hash

--- app/src/main/java/com/udacity/notepad/data/Note.kt
+++ app/src/main/java/com/udacity/notepad/data/Note.kt
@@ -2,10 +2,16 @@ package com.udacity.notepad.data
 
 import java.util.Date
 
-class Note {
-    var id = -1
-    var text: String? = null
-    var isPinned = false
-    var createdAt = Date()
+data class Note (
+    var id: Int = -1,
+    var text: String? = null,
+    var isPinned: Boolean = false,
+    var createdAt: Date = Date(),
     var updatedAt: Date? = null
-}
+)
+
+/*
+    val note = Note(text = "This is a note")
+    print(note == note.copy())  // True
+    val aCopy = note.copy(text = "This is a copy", isPinned = true)
+ */
